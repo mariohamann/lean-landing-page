2c8d1d9ce13e72c188e069fd29fe7b403a4236a9
Mario Hamann <mariovhamann@gmail.com>
2023-03-10 22:18:04 +0100

    feat: add astro compression

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,5 @@
 import { defineConfig } from 'astro/config';
+import compress from "astro-compress";
 
 // https://astro.build/config
-export default defineConfig({});
+export default { integrations: [compress()] };
