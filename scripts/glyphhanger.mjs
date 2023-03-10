4ffef6e9e5b412abe1197435ce50300e5402f14f
Mario Hamann <mariovhamann@gmail.com>
2023-03-11 00:12:24 +0100

    feat: integrate font subsetting

---
+++ scripts/glyphhanger.mjs
@@ -0,0 +1,32 @@
+import { execSync } from 'child_process';
+import fs from 'fs';
+
+// Define the command to run
+const fontsDir = './dist/fonts';
+const command = `glyphhanger ./dist/index.html --string --jsdom --cssSelector=".font-fira, .font-fira *" --subset=${fontsDir}/*.woff2 --output=${fontsDir}`;
+// Run the command
+console.log('Running command...');
+execSync(command, { stdio: 'inherit' });
+
+// Remove the original files
+console.log('Removing original files...');
+fs.readdirSync(fontsDir).forEach((filename) => {
+  if (filename.endsWith('.woff2') && !filename.includes('subset')) {
+    fs.rmSync(`${fontsDir}/${filename}`);
+  }
+});
+
+// Rename the subset files to the original name
+console.log('Renaming subset files...');
+fs.readdirSync(fontsDir).forEach((filename) => {
+  if (filename.endsWith('-subset.woff2')) {
+    const originalFilename = filename.replace('-subset', '');
+    fs.renameSync(`${fontsDir}/${filename}`, `${fontsDir}/${originalFilename}`);
+  }
+  if (filename.endsWith('-subset.zopfli.woff')) {
+    const originalFilename = filename.replace('-subset.zopfli', '');
+    fs.renameSync(`${fontsDir}/${filename}`, `${fontsDir}/${originalFilename}`);
+  }
+});
+
+console.log('Done.');
